20cb670c1ed5fc6bec3643a63de2693cbfd758a3
cod3breaker <ravisherath@gmail.com>
2020-03-08 13:31:55 +1100

    adding store manager

---
+++ DataHandler/Store/StoreManager.swift
@@ -0,0 +1,15 @@
+//
+//  StoreManager.swift
+//  DataHandler
+//
+//  Created by Sunimal Herath on 8/3/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import Foundation
+
+class StoreManager {
+    static let instance = StoreManager()
+    
+    var userProfileStore = UserProfileStore()
+}

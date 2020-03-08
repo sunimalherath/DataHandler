30d35f5caf6f7d204e411cfc22a229d9de8296e8
cod3breaker <ravisherath@gmail.com>
2020-03-08 13:15:13 +1100

    adding profile screen

---
+++ DataHandler/Screens/ProfileScreen.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileScreen.swift
+//  DataHandler
+//
+//  Created by Sunimal Herath on 8/3/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileScreen()
+    }
+}
